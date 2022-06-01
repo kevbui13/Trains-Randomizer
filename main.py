9a0ebe5872d0ab0c70fcd1a959286985be45ce25
Kevin Bui <94651800+kevbui13@users.noreply.github.com>
2022-06-01 10:47:54 -0700

    Update main.py
    Additional comments.

--- main.py
+++ main.py
@@ -10,16 +10,17 @@ card_name = []
 
 if __name__ == '__main__':
 
-    ## Randomizers Random Mechanic
+    ## Randomizers - Random Mechanic
     while error != 0:
         error = 0
         pool = random.sample(randomizer, k=8)
 
+        ## Define three lists
         card_cost = []
         card_type = []
         card_name = []
 
-        ## String Manipulations
+        ## String Manipulations - to indicate card cost, card type, and card name
         for i in range(0,8):
             data = str(pool[i])
             card_cost.append((int(data[0])))
@@ -27,10 +28,13 @@ if __name__ == '__main__':
             card_name.append(data[2:])
 
         ## Checks for Balanced Randomizers
+        
+        ## Checks the supply cost sum
         check_sum = sum(card_cost)
         if check_sum > 44 or check_sum < 34:
             error = error + 1
 
+        ## Checks the card type distribution
         check_type = card_type.count("r")
         if check_type > 5 or check_type < 1:
             error = error + 1
